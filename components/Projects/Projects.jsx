29fc4649f28999bef57e7aa35b569b825aff7d21
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-15 23:21:53 +0530

    ADD descriptions for projects

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,90 +1,27 @@
+import { projects } from "@/pages/api/projectsAPI";
 import React, { useState } from "react";
 import { ReactDOM } from "react";
 import styles from "./Projects.module.css";
 
+function GridItem(props) {
+  return (
+    <div className={styles.gridItem}>
+      <img src={props.source} className={styles.projectImage} alt="" />
+      <div className={styles.projectName}>{props.name}</div>
+    </div>
+  );
+}
+
 export default function Projects() {
   const [imageNo, setImageNo] = useState(0);
+  const gridItems = projects.map((project) => {
+    return <GridItem {...project} />;
+  });
   return (
     <>
       <div className={styles.projects}>
         <h1 className={styles.header}>My Projects</h1>
-        <div className={styles.projectsGrid}>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${0}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Opencademy</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${1}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Youtify</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${2}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Memory Card Game</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${3}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Battleship Game Simulation</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${4}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Todo List</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${5}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Library</div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${6}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>
-              Cat Themed Dashboard Project
-            </div>
-          </div>
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${7}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Rock Paper Scissor</div>
-          </div>
-
-          <div className={styles.gridItem}>
-            <img
-              src={`/project${8}.webp`}
-              className={styles.projectImage}
-              alt=""
-            />
-            <div className={styles.projectName}>Drum Simulation</div>
-          </div>
-        </div>
+        <div className={styles.projectsGrid}>{gridItems}</div>
       </div>
     </>
   );
---
+++ src/pages/api/projectsAPI.js
@@ -0,0 +1,66 @@
+export const projects = [
+  {
+    name: "Youtify",
+    source: "/project0.webp",
+    description:
+      "A Youtube clone with spotify UI. Uses Youtube Data API for fetching songs and react-youtube library for embedding the video into the website. You can also search songs and view trending songs based on your location.",
+    ghLink: "",
+  },
+  {
+    name: "Opencademy",
+    source: "/project1.webp",
+    description:
+      "A prototype for an open-source education platform. Built at NITR hackathon over the span of 36 hours. We used next.js to build this and uses Ant design as its frontend library.",
+    ghLink: "",
+  },
+  {
+    name: "Memory Card Game",
+    source: "/project2.webp",
+    description:
+      "This memory-enhancing game utilizes an API to retrieve anime data, tasking players with correctly identifying and selecting the shuffled options. Uses react as it's front library.",
+    ghLink: "",
+  },
+  {
+    name: "Battleship Game Simulation",
+    source: "/project3.webp",
+    description:
+      "This game is a re-imagination of the classic Battleship game, where players aim to locate all enemy ships before their own ships are discovered. It is developed using Vanilla Javascript, with a focus on utilizing Test Driven Development techniques.",
+    ghLink: "",
+  },
+  {
+    name: "Todo List",
+    source: "/project4.webp",
+    description:
+      "This Todo-List application utilizes the Web Storage API to store tasks in a local database. It features the ability to organize tasks by project and includes functionalities such as tabbing and the ability to create separate task lists for each project. It was developed as a means to gain practical experience with Webpack and Factory Function techniques.",
+    ghLink: "",
+  },
+
+  {
+    name: "Library",
+    source: "/project5.webp",
+    description:
+      "This application was constructed using Vanilla Javascript, with a primary focus on learning form validation and grid layout. Users can add books to their personal library and mark them as read or unread.",
+    ghLink: "",
+  },
+  {
+    name: "Cat Themed Dashboard Project",
+    source: "/project6.webp",
+    description:
+      "A cat themed dashboard projects with a sprinkle of creativity. Uses grid layouts and a lot of css.",
+    ghLink: "",
+  },
+  {
+    name: "Rock Paper Scissor",
+    source: "/project7.webp",
+    description:
+      "Classic rock paper scissor game made using html css and javascript. Played against a computer which selects rock paper or scissor based on Math.random().",
+    ghLink: "",
+  },
+  {
+    name: "Drum Simulation",
+    source: "/project8.webp",
+    description:
+      "My first ever web development project. Plays drum sounds when the user presses any key shown the screen. Uses event listeners to capture the inputted key which then plays a sound it's associated with.",
+    ghLink: "",
+  },
+];
