6b67464a0906bf387124b65ef528c425f0ff8d13
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-15 11:31:38 +0530

    Design navbar

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -3,5 +3,9 @@ import React from "react";
 import { ReactDOM } from "react";
 
 export default function NavBar() {
-  return <div className={styles.navBarContainer}></div>;
+  return (
+    <div className={styles.navBarContainer}>
+      <div className={styles.navBar}></div>
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Inter } from "@next/font/google";
 import AboutMe from "components/AboutMe/AboutMe";
+import NavBar from "components/NavBar/NavBar";
 
 import styles from "../styles/Home.module.css";
 
@@ -7,8 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={styles.sectionsContainer}>
-      <AboutMe />
+    <div className={styles.body}>
+      <div className={styles.sectionsContainer}>
+        <AboutMe />
+        <AboutMe />
+      </div>
+      <NavBar />
     </div>
   );
 }
