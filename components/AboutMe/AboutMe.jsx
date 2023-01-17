b1d28365656710e574e4e5c5ee9a9a769908aca4
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-17 09:59:05 +0530

    Fixed all current bugs

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { ReactDOM } from "react";
 import styles from "./AboutMe.module.css";
@@ -12,10 +13,31 @@ export default function AboutMe() {
             Hello! I'm currently in my 3rd semester pursuing computer science. I
             love programming and learning new technologies.
           </div>
+          <div className={styles.logoContainer}>
+            <Link href="https://github.com/vbv-nyk" target="_blank">
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                alt=""
+                className={styles.logoItem}
+              />
+            </Link>
+            <Link href="https://devfolio.co/@vbvnyk" target="_blank">
+              <img
+                src="https://avatars.githubusercontent.com/u/38809367?s=280&v=4"
+                alt=""
+                className={styles.logoItem}
+              />
+            </Link>
+            <Link href="https://www.codechef.com/users/vbv_nyk" target="_blank">
+              <img
+                src="https://pbs.twimg.com/profile_images/1477930785537605633/ROTVNVz7_400x400.jpg"
+                alt=""
+                className={styles.logoItem}
+              />
+            </Link>
+          </div>
         </div>
       </div>
-      <div className={styles.githubContainer}></div>
-      <div className={styles.devfolioContainer}></div>
     </div>
   );
 }
