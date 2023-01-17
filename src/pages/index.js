b1655e5490ae63a44563a0d8dbe1de491fc12b3f
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-17 10:03:12 +0530

    Scrolling on android now stops on the next grid item

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,16 @@ import ContactPage from "components/ContactPage/ContactPage";
 import NavBar from "components/NavBar/NavBar";
 import Projects from "components/Projects/Projects";
 import { useEffect, useState } from "react";
-import Swipeable, { useSwipeable } from "react-swipeable";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState(0);
   const pages = ["#about", "#projects", "#contact"];
-  const handlers = useSwipeable({
-    onSwipedLeft: (eventData) => {
-      window.location.href = pages[(currentPage + 1) % 3];
-      setCurrentPage((n) => (n + 1) % 3);
-    },
-    onSwipedRight: (eventData) => {
-      if (currentPage === 0) {
-        window.location.href = pages[2];
-        setCurrentPage(2);
-      } else {
-        window.location.href = pages[currentPage - 1];
-        setCurrentPage((n) => n - 1);
-      }
-    },
-  });
   return (
     <>
       <NavBar />
-      <div className={styles.sectionsContainer} {...handlers}>
+      <div className={styles.sectionsContainer}>
         <AboutMe />
         <Projects />
         <ContactPage />
